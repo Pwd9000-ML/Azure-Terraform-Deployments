71ee768bfe7250604affa413845dad2ddd3e6419
M L <magiconion_M@hotmail.com>
2022-01-22 12:54:06 +0000

    update workflows

--- 03_etc_etc/another_terraform_module.tf
+++ 03_etc_etc/another_terraform_module.tf
@@ -1,2 +1,17 @@
+terraform {
+  required_version = ">= 1.1.3"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.93"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
 ### Another awesome Terraform ROOT module ###
 ### Let's take a break and play some Gwent ###
