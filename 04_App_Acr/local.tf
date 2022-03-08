4c788a3cf605a3958d211e54a6567f9e2035443f
M L <magiconion_M@hotmail.com>
2022-03-08 17:18:32 +0000

    test

--- 04_App_Acr/local.tf
+++ 04_App_Acr/local.tf
@@ -1,15 +1,16 @@
 locals {
   #Appsvc Settings
   app_settings = {
-    default_settings = {
+    default_settings = sensitive({
       APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.INSIGHTS.instrumentation_key}"
       DOKCER_REGISTRY_SERVER_URL     = "${var.acr_name}.azurecr.io"
-    },
-    linux_app_settings = {
+    }),
+    linux_app_settings = sensitive({
       APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.INSIGHTS.instrumentation_key}"
       DOKCER_REGISTRY_SERVER_URL     = "${var.acr_name}.azurecr.io"
       WEBSITE_PULL_IMAGE_OVER_VNET   = "true"
-    }
+      LINUX_SENSITIVE_VALUE          = "!!sensitive_value!!"
+    })
   }
   linux_fx_version = "DOCKER|${var.acr_name}/${var.appsvc_name}:latest"
 }
