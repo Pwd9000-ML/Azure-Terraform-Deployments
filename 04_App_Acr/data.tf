e09dab59c21303434cc6f5b0c51fb38b4b58f998
M L <magiconion_M@hotmail.com>
2022-03-22 13:55:33 +0000

    test

--- 04_App_Acr/data.tf
+++
@@ -1,8 +0,0 @@
-## Get subnet Ids to create NSGs for each subnet
-data "azurerm_subnet" "network_subs" {
-  count                = length(azurerm_virtual_network.VNET.subnets)
-  name                 = azurerm_virtual_network.VNET.subnets[count.index]
-  virtual_network_name = azurerm_virtual_network.VNET.name
-  resource_group_name  = azurerm_resource_group.RG.name
-  depends_on           = [azurerm_virtual_network.VNET, azurerm_subnet.SUBNETS]
-}
--- 04_App_Acr/vnet.tf
+++ 04_App_Acr/vnet.tf
@@ -36,3 +36,16 @@ resource "azurerm_subnet" "SUBNETS" {
     }
   }
 }
+
+resource "azurerm_network_security_group" "NSG" {
+  count               = length(azurerm_virtual_network.VNET.subnet.*.id)
+  name                = "${azurerm_virtual_network.VNET.subnet[count.index].name}-nsg"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_subnet_assoc" {
+  count                     = length(azurerm_virtual_network.VNET.subnet.*.id)
+  subnet_id                 = azurerm_virtual_network.VNET.subnet[count.index].id
+  network_security_group_id = azurerm_network_security_group.NSG[count.index].id
+}
