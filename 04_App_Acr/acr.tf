7036dcc96b12ec1490546628e3aabd305d48d184
M L <magiconion_M@hotmail.com>
2022-03-29 11:07:37 +0100

    fixes

--- 04_App_Acr/acr.tf
+++ 04_App_Acr/acr.tf
@@ -20,27 +20,6 @@ resource "azurerm_container_registry" "ACR" {
     }
   }
 
-  #  dynamic "network_rule_set" {
-  #    for_each = local.acr_fw_rules != null ? local.acr_fw_rules : []
-  #    content {
-  #      default_action = network_rule_set.value.default_action
-  #      dynamic "ip_rule" {
-  #        for_each = network_rule_set.value["ip_rules"] != [] ? network_rule_set.value["ip_rules"] : []
-  #        content {
-  #          action   = ip_rule.value["action"]
-  #          ip_range = ip_rule.value["ip_range"]
-  #        }
-  #      }
-  #     dynamic "virtual_network" {
-  #       for_each = network_rule_set.value["virtual_network_subnets"] != [] ? network_rule_set.value["virtual_network_subnets"] : []
-  #       content {
-  #         action    = virtual_network.value["action"]
-  #         subnet_id = virtual_network.value["subnet_id"]
-  #       }
-  #     }
-  #   }
-  # }
-
   network_rule_set = [
     {
       default_action = var.acr_network_rule_set_default_action
