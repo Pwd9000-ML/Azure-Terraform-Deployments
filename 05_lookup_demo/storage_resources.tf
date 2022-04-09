ac56168c1f140a5e0da4bb36b68ce5070c6c092d
M L <magiconion_M@hotmail.com>
2022-04-09 11:50:40 +0100

    fix

--- 05_lookup_demo/storage_resources.tf
+++ 05_lookup_demo/storage_resources.tf
@@ -2,7 +2,7 @@
 # RESOURCES                                      #
 ##################################################
 resource "azurerm_resource_group" "RGS" {
-  for_each = var.site_names
+  for_each = toset(var.site_names)
   name     = lookup(local.site_configs, each.value["resource_group_name"], null)
   location = lookup(local.site_configs, each.value["location"], null)
 }
