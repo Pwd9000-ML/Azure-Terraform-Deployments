07566561e33228d3a161d3dbe44a56915757c71c
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 03:59:24 +0000

    Update role assignment count logic in foundation_resources.tf

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -20,7 +20,7 @@ resource "azurerm_user_assigned_identity" "uai" {
 }
 
 resource "azurerm_role_assignment" "rbac" {
-  count                = 2
+  count                = true ? 1 : 0
   principal_id         = azurerm_user_assigned_identity.uai.principal_id
   role_definition_name = "Contributor"
   scope                = azurerm_resource_group.rg.id
