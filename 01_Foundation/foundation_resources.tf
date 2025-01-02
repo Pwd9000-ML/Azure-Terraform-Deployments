76f7503095e61c552f5eeff9b2ff9fb3fed4755b
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 10:29:12 +0000

    Refactor role assignment logic in foundation_resources.tf to dynamically create assignments based on existing roles

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -19,10 +19,20 @@ resource "azurerm_user_assigned_identity" "uai" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_role_assignment" "rbac" {
-  count                = true ? 1 : 0
+# Check if the role assignment already exists with 'for_each' on  'data' resource
+data "azurerm_role_assignments" "rbac" {
+  for_each             = toset(["Contributor", "Reader"])
   principal_id         = azurerm_user_assigned_identity.uai.principal_id
-  role_definition_name = "Contributor"
+  role_definition_name = each.value
+  scope                = azurerm_resource_group.rg.id
+}
+
+# Only create role assignments for the role definitions that do not exist in the data resource check and skip the ones that already exist in the data resource check
+resource "azurerm_role_assignment" "rbac" {
+  for_each             = toset(["Contributor", "Reader"])
+  count                = data.azurerm_role_assignments.rbac[each.value] == null ? 1 : 0
+  principal_id         = azurerm_user_assigned_identity.uai.principal_id
+  role_definition_name = each.value
   scope                = azurerm_resource_group.rg.id
 }
 
