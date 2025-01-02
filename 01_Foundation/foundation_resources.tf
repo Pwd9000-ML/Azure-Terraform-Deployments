88578b3aba788ddcf9f994851f4784b2afec965d
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 02:30:07 +0000

    Add user assigned managed identity and update role assignment for Contributor role

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -12,9 +12,16 @@ resource "azurerm_resource_group" "rg" {
   tags     = var.tags
 }
 
-resource "azurerm_role_assignment" "example" {
-  count                = 2
-  principal_id         = data.azurerm_client_config.current.object_id
+#Write a resource creation of a user assigned managed identity
+resource "azurerm_user_assigned_identity" "uai" {
+  name                = "${var.resource_group_name}-uai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_role_assignment" "rbac" {
+  count = 2
+  principal_id         = azurerm_user_assigned_identity.uai.principal_id
   role_definition_name = "Contributor"
   scope                = azurerm_resource_group.rg.id
 }
