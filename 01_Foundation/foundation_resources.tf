50fd94ab0661fb02704dd9caea43dc4627839e99
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 15:19:13 +0000

    Add import block for existing role assignments in foundation_resources.tf

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -27,17 +27,13 @@ resource "azurerm_user_assigned_identity" "uai" {
 #  }
 #}
 
-# Use the import block to declare the existing role assignments to import 
-import {
-  to = azurerm_role_assignment.rbac
-  id = "/subscriptions/829efd7e-aa80-4c0d-9c1c-7aa2557f8e07/resourceGroups/Demo-Inf-Dev-Rg/providers/Microsoft.Authorization/roleAssignments/1a533459-6925-4770-9c4e-0d341ae69691"
-}
+
 
 # Create the azurerm_role_assignment resource importing the existing role assignments
 resource "azurerm_role_assignment" "rbac" {
-  principal_id         = azurerm_user_assigned_identity.uai.principal_id
-  role_definition_name = "Contributor"
-  scope                = azurerm_resource_group.rg.id
+  principal_id = azurerm_user_assigned_identity.uai.principal_id
+  #role_definition_name = "Contributor"
+  scope = azurerm_resource_group.rg.id
   #import {
   #  id = each.value
   #}
