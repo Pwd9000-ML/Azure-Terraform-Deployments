944c4e92fc4900d51c3237a17d4dfb7d96fe5940
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 15:44:51 +0000

    up

--- 01_Foundation/imports.tf
+++
@@ -1,5 +0,0 @@
-# Use the import block to declare the existing role assignments to import 
-import {
-  to = azurerm_role_assignment.rbac
-  id = "/subscriptions/829efd7e-aa80-4c0d-9c1c-7aa2557f8e07/resourceGroups/Demo-Inf-Dev-Rg/providers/Microsoft.Authorization/roleAssignments/1a533459-6925-4770-9c4e-0d341ae69691"
-}
