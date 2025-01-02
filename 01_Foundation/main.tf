5b8ee5102869ee1f3db05357f200962e0fbfef64
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-02 14:41:24 +0000

    Update Terraform required version and azurerm provider version in main.tf

--- 01_Foundation/main.tf
+++ 01_Foundation/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.10.0"
+  required_version = ">= 1.10.3"
   backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.14"
     }
   }
 }
