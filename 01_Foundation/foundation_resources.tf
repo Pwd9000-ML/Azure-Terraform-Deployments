4afcdf58c9e930a45d727fdaf7a70293d2804077
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-01-03 00:47:07 +0000

    Remove unused import block for existing role assignments in imports.tf

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -19,45 +19,6 @@ resource "azurerm_user_assigned_identity" "uai" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create a locals map of the RBAC permissions on the Resource Group level
-locals {
-  role_assignments = {
-    Reader      = "/subscriptions/829efd7e-aa80-4c0d-9c1c-7aa2557f8e07/resourceGroups/Demo-Inf-Dev-Rg/providers/Microsoft.Authorization/roleAssignments/d5ee3efa-0ebe-44b7-a6ff-cdf1abc64418",
-    Contributor = "/subscriptions/829efd7e-aa80-4c0d-9c1c-7aa2557f8e07/resourceGroups/Demo-Inf-Dev-Rg/providers/Microsoft.Authorization/roleAssignments/511b6d94-4d69-41bd-898d-1d6ce49a9834"
-  }
-}
-
-import {
-  for_each = local.role_assignments
-  to       = azurerm_role_assignment.rbac[each.key]
-  id       = each.value
-}
-
-# Create the azurerm_role_assignment resource importing the existing role assignments
-resource "azurerm_role_assignment" "rbac" {
-  for_each             = local.role_assignments
-  principal_id         = azurerm_user_assigned_identity.uai.principal_id
-  role_definition_name = each.key
-  scope                = azurerm_resource_group.rg.id
-}
-
-
-# # Create a null resource with a local-exec provisioner to create the role assignment for contributor and reader from a var.permissions list
-# resource "null_resource" "rbac" {
-#   for_each = toset(["Contributor", "Reader"])
-#   triggers = {
-#     always_run = timestamp()
-#   }
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       az login --service-principal --username $ARM_CLIENT_ID --password $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID --output none
-#       az account set --subscription $ARM_SUBSCRIPTION_ID --output none
-#       az role assignment create --assignee ${azurerm_user_assigned_identity.uai.principal_id} --role ${each.key} --scope ${azurerm_resource_group.rg.id}
-#     EOT
-#   }
-# }
-
 #Create a Key Vault for the Resource Group
 resource "azurerm_key_vault" "kv" {
   name                        = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
