56c3ceae204ed8f6a5d68cb188964dcb8a17a366
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-02-21 12:04:05 +0000

    Refactor resource group creation logic to use local variable for clarity and efficiency

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -10,19 +10,24 @@ data "azurerm_resource_group" "rg" {
   name = "Demo-Inf-Dev-Rg-720"
 }
 
-# Only create the resource group if it does not exist in the data resource check
+# Determine whether to create the resource group using terraform or use the existing resource group ID if it exists
+locals {
+  create_rg = length(try(data.azurerm_resource_group.rg.id, "")) == 0 ? toset(["Demo-Inf-Dev-Rg-720"]) : toset([])
+}
+
+# Only create the resource group if it does not exist otherwise use the existing resource group ID
 resource "azurerm_resource_group" "rg" {
-  for_each = data.azurerm_resource_group.rg == null ? toset(["Demo-Inf-Dev-Rg-720"]) : toset([])
+  for_each = local.create_rg
   name     = each.key
   location = "East US"
 }
 
 # Create other resources that depend on the resource group 
 resource "azurerm_storage_account" "sa" {
-  for_each                 = data.azurerm_resource_group.rg == null ? toset(["Demo-Inf-Dev-Rg-720"]) : toset([])
+  for_each                 = local.create_rg
   name                     = "demoinfdevsa720"
-  resource_group_name      = azurerm_resource_group.rg[each.key].name
-  location                 = azurerm_resource_group.rg[each.key].location
+  resource_group_name      = coalesce(try(data.azurerm_resource_group.rg.name, null), try(azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].name, null))
+  location                 = coalesce(try(data.azurerm_resource_group.rg.location, null), try(azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].location, null))
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
