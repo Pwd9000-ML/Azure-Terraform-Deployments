895bfdc7838bf8d5131c1562bcf6317d4c9811c3
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-02-21 16:23:37 +0000

    Refactor resource group existence check to use Azure CLI and improve reliability

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -5,38 +5,31 @@ data "azurerm_client_config" "current" {}
 # FOUNDATIONAL RESOURCES                         #
 ##################################################
 
-# Check if the resource group already exists
-data "azurerm_resource_group" "rg_existing" {
-  name = "Demo-Inf-Dev-Rg-720"
-}
+# Check if the resource group already exists with 'terraform_data' resource
+resource "terraform_data" "rg_existing" {
+  triggers_replace = timestamp()
 
-# Determine whether the resource group exists
-locals {
-  rg_exists = can(data.azurerm_resource_group.rg_existing.id) # Returns true if RG exists
+  provisioner "local-exec" {
+    # Use the Azure CLI to check if the resource group exists
+    command = "az group show --name 'Demo-Inf-Dev-Rg-720' --query id"
+  }
 }
 
 # Only create the resource group if it does not exist
 resource "azurerm_resource_group" "rg" {
-  count    = local.rg_exists ? 0 : 1
+  count    = terraform_data.rg_existing.result == "" ? 1 : 0
   name     = "Demo-Inf-Dev-Rg-720"
-  location = "East US"
+  location = "UKSouth"
 }
 
-# Create the storage account and reference the correct RG
+# Create the storage account in the resource group
 resource "azurerm_storage_account" "sa" {
-  name = "demoinfdevsa720"
-
-  # Use existing RG from data source if available, otherwise use the newly created RG
-  resource_group_name = local.rg_exists ? data.azurerm_resource_group.rg_existing.name : azurerm_resource_group.rg[0].name
-  location            = local.rg_exists ? data.azurerm_resource_group.rg_existing.location : azurerm_resource_group.rg[0].location
-
+  resource_group_name      = "Demo-Inf-Dev-Rg-720"
+  name                     = "demoinfdevsa720"
+  location                 = "UKSouth"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  # Ensure Terraform waits for RG creation if needed
-  depends_on = [azurerm_resource_group.rg]
 }
-
 
 
 # #Create a Resource Group
