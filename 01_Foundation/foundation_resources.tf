a2fa3b4c36d9609f08c7d2eb6c329b624a46e468
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-02-21 11:16:03 +0000

    Refactor resource group and storage account definitions for clarity and consistency

--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -5,26 +5,26 @@ data "azurerm_client_config" "current" {}
 # FOUNDATIONAL RESOURCES                         #
 ##################################################
 
-  # Check if the resource group already exists with 'data' resource
-  data "azurerm_resource_group" "rg" {
-    name = "Demo-Inf-Dev-Rg-720"
-  }
+# Check if the resource group already exists with 'data' resource
+data "azurerm_resource_group" "rg" {
+  name = "Demo-Inf-Dev-Rg-720"
+}
 
- # Only create the resource group if it does not exist in the data resource check
-  resource "azurerm_resource_group" "rg" {
-    for_each = data.azurerm_resource_group.rg == null ? toset(["Demo-Inf-Dev-Rg-720"]) : toset([])
-    name     = each.key
-    location = "East US"
-  }
+# Only create the resource group if it does not exist in the data resource check
+resource "azurerm_resource_group" "rg" {
+  for_each = data.azurerm_resource_group.rg == null ? toset(["Demo-Inf-Dev-Rg-720"]) : toset([])
+  name     = each.key
+  location = "East US"
+}
 
- # Create other resources that depend on the resource group 
-  resource "azurerm_storage_account" "sa" {
-    name                = "demoinfdevsa720"
-    resource_group_name = azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].name
-    location            = azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].location
-    account_tier        = "Standard"
-    account_replication_type = "LRS"
-  }
+# Create other resources that depend on the resource group 
+resource "azurerm_storage_account" "sa" {
+  name                     = "demoinfdevsa720"
+  resource_group_name      = azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].name
+  location                 = azurerm_resource_group.rg["Demo-Inf-Dev-Rg-720"].location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 
 # #Create a Resource Group
