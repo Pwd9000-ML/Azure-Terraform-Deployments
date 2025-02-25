cd426a649f5873c13099cfed812f44c1f09d5187
Pwd9000-ML <pwd9000@hotmail.co.uk>
2025-02-25 18:07:32 +0000

    Refactor resource group creation by removing redundant checks and updating naming conventions

--- 01_Foundation/config-dev.tfvars
+++ 01_Foundation/config-dev.tfvars
@@ -1,4 +1,4 @@
-resource_group_name = "Demo-Inf-Dev-Rg-720"
+resource_group_name = "Demo-Inf-Dev-Rg"
 location            = "UKSouth"
 key_vault_name      = "Pwd9000-Inf-Dev-Kv"
 use_rbac_mode       = true
--- 01_Foundation/foundation_resources.tf
+++ 01_Foundation/foundation_resources.tf
@@ -4,85 +4,35 @@ data "azurerm_client_config" "current" {}
 ##################################################
 # FOUNDATIONAL RESOURCES                         #
 ##################################################
-
-# Check if the resource group already exists with 'terraform_data' resource
-resource "terraform_data" "rg_check" {
-  input            = var.resource_group_name
-  triggers_replace = timestamp()
-
-  provisioner "local-exec" {
-    # Use the Azure CLI with PowerShell Core to check if the resource group exists adn create if not
-    interpreter = ["pwsh", "-Command"]
-    command     = <<EOT
-      $ErrorActionPreference = "Stop"
-      az login --service-principal --username $env:ARM_CLIENT_ID --password $env:ARM_CLIENT_SECRET --tenant $env:ARM_TENANT_ID --output none
-      az account set --subscription $env:ARM_SUBSCRIPTION_ID --output none
-      $rg_verify = az group show --name ${var.resource_group_name} --query id --output tsv
-      if ($rg_verify) {
-        $rg_exist = Write-Output "$rg_verify"
-      } else {
-        $rg_create = (az group create --name ${var.resource_group_name} --location ${var.location} --query id --output tsv)
-      }
-    EOT
-  }
+#Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
 }
 
-output "rg_id_output" {
-  description = "The ID of the resource group that was created or already existed"
-  value       = terraform_data.rg_check.output
+#Write a resource creation of a user assigned managed identity
+resource "azurerm_user_assigned_identity" "uai" {
+  name                = "${var.resource_group_name}-uai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# # The verification test will return the resource ID regardless of whether the resource group was created or already existed
-# import {
-#   to = azurerm_resource_group.rg
-#   id = module.verify.rg_id_output
-# }
+#Create a Key Vault for the Resource Group
+resource "azurerm_key_vault" "kv" {
+  name                        = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enable_rbac_authorization   = var.use_rbac_mode
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  tags                        = var.tags
+}
 
-# # Resource Group block is required to import the resource group ID from the verify module
-# resource "azurerm_resource_group" "rg" {
-#   name     = "Demo-Inf-Dev-Rg-720"
-#   location = "UKSouth"
-# }
-
-# # Create the storage account in the resource group
-# resource "azurerm_storage_account" "sa" {
-#   resource_group_name      = "Demo-Inf-Dev-Rg-720"
-#   name                     = "demoinfdevsa720"
-#   location                 = "UKSouth"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
-
-
-# #Create a Resource Group
-# resource "azurerm_resource_group" "rg" {
-#   name     = var.resource_group_name
-#   location = var.location
-#   tags     = var.tags
-# }
-
-# #Write a resource creation of a user assigned managed identity
-# resource "azurerm_user_assigned_identity" "uai" {
-#   name                = "${var.resource_group_name}-uai"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
-
-# #Create a Key Vault for the Resource Group
-# resource "azurerm_key_vault" "kv" {
-#   name                        = "${lower(var.key_vault_name)}${random_integer.kv_num.result}"
-#   location                    = azurerm_resource_group.rg.location
-#   resource_group_name         = azurerm_resource_group.rg.name
-#   enable_rbac_authorization   = var.use_rbac_mode
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = data.azurerm_client_config.current.tenant_id
-#   soft_delete_retention_days  = 7
-#   purge_protection_enabled    = false
-#   sku_name                    = "standard"
-#   tags                        = var.tags
-# }
-
-# resource "random_integer" "kv_num" {
-#   min = 0001
-#   max = 9999
-# }
+resource "random_integer" "kv_num" {
+  min = 0001
+  max = 9999
+}
